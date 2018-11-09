04dc692dace7b7260382eec7344628cee36d5d73
ddSoul <972424506@qq.com>
2018-11-09 11:02:52 +0800

    gitignore

---
+++ FastLane/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FastLane
+//
+//  Created by ddSoul on 2018/11/9.
+//  Copyright © 2018 dxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

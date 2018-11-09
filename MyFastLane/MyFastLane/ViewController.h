6e572f56b28cbfcd621100a289bcb0e563704028
ddSoul <972424506@qq.com>
2018-11-09 16:49:23 +0800

    action

---
+++ MyFastLane/MyFastLane/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFastLane
+//
+//  Created by ddSoul on 2018/11/9.
+//  Copyright © 2018 dxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyFastLane/MyFastLane/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  MyFastLane
+//
+//  Created by ddSoul on 2018/11/9.
+//  Copyright © 2018 dxl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = UIColor.whiteColor;
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
